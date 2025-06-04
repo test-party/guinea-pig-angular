c82c83adc0096149b190e1ce9ff933192f9c5087
alexander <alexander@testparty.ai>
2025-06-04 17:22:24 -0400

    amk - document title page

---
+++ src/app/pages/document-title/document-title.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-title',
+  standalone: true,
+  imports: [],
+  templateUrl: './document-title.component.html',
+  styles: ``
+})
+export class DocumentTitleComponent {
+
+}
