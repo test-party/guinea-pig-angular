3062b6aef6fa57adc45de047ed9543b5d6e72754
Artur Meinen <arturmeinen@gmail.com>
2025-06-07 21:41:20 -0300

    test

--- src/app/pages/issues/issues.component.ts
+++ src/app/pages/issues/issues.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-issues',
   standalone: true,
-  imports: [CommonModule], 
+  imports: [CommonModule,FormsModule], 
   templateUrl: './issues.component.html'
 })
 export class IssuesComponent {
   readonly show = false;
+  showHidden = false;
+  items = [1, 2, 3];
+  inputValue = '';
 }
