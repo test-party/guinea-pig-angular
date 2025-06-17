25275a63fefbc897d69903f1c86a0648f9b07bd0
Artur Meinen <arturmeinen@gmail.com>
2025-06-17 15:20:14 -0300

    test

--- src/app/pages/thirdparty-page/thirdparty-page.component.ts
+++ src/app/pages/thirdparty-page/thirdparty-page.component.ts
@@ -4,11 +4,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { NbCardModule } from '@nebular/theme';
+import { NbTabsetModule } from '@nebular/theme';
 
 @Component({
   selector: 'app-thirdparty-page',
   standalone: true,
-  imports: [CommonModule,FontAwesomeModule,FaIconComponent,NbCardModule],
+  imports: [CommonModule,FontAwesomeModule,FaIconComponent,NbCardModule,NbTabsetModule],
   templateUrl: './thirdparty-page.component.html',
 })
 export class ThirdpartyPageComponent {
