9ccdd9ae4ca249d6569871cc17df2f5be58b2915
Artur Meinen <arturmeinen@gmail.com>
2025-06-17 11:58:10 -0300

    configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NbLayoutModule, NbIconModule } from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NbLayoutModule, NbIconModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'guinea-pig-angular';
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
+// main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { NbThemeModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      NbThemeModule.forRoot({ name: 'default' }),
+      NbEvaIconsModule
+    ),
+  ],
 }).catch(err => console.error(err));
