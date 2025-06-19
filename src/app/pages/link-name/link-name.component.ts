99c501171d2791064d5df7ea5816766ad90a3823
Artur Meinen <arturmeinen@gmail.com>
2025-06-18 22:49:39 -0300

    fix unrelated

--- src/app/pages/link-name/link-name.component.ts
+++ src/app/pages/link-name/link-name.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-link-name',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule],
   templateUrl: './link-name.component.html',
   styles: ``
 })
 export class LinkNameComponent {
-  items = ['Item 1', 'Item 2'];
 }
