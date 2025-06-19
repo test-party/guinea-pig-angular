f1a7754aea1d0a3d35730427ac21b1b0419b9bfa
Artur Meinen <arturmeinen@gmail.com>
2025-06-18 22:48:04 -0300

    fix unrelated

--- src/app/pages/issues/import/import.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-import-issues',
-  standalone: true,
-  templateUrl: './import.component.html'
-})
-export class ImportComponent {}
