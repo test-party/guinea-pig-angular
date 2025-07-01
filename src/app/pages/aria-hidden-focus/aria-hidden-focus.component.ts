0b013adfda7bfb507fa9bd146e0c68dea3565d82
Christian <167792134+christian-testparty@users.noreply.github.com>
2025-07-01 14:46:33 -0400

    create aria-hidden-focus page

---
+++ src/app/pages/aria-hidden-focus/aria-hidden-focus.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aria-hidden-focus',
+  standalone: true,
+  imports: [],
+  templateUrl: './aria-hidden-focus.component.html',
+  styles: ``
+})
+export class AriaHiddenFocusComponent {
+
+}
