24db5d32d836265762e47f8791eab03269e766da
alexander <alexander@testparty.ai>
2025-07-01 17:56:21 -0400

    amk - defintion list page

---
+++ src/app/pages/definition-list/definition-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-definition-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './definition-list.component.html',
+  styles: ``
+})
+export class DefinitionListComponent {
+
+}
