5474fa644caf6fbddcf487d2e32d4de1ce23d5bb
alexander <alexander@testparty.ai>
2025-07-02 17:26:51 -0400

    amk - dlitem page

---
+++ src/app/pages/dlitem/dlitem.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dlitem',
+  standalone: true,
+  imports: [],
+  templateUrl: './dlitem.component.html',
+  styles: ``
+})
+export class DlItemComponent {
+
+}
