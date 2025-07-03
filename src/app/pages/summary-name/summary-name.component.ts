2f6f40839aff27ecebc6a33d6744cb740f92f7f3
Christian <167792134+christian-testparty@users.noreply.github.com>
2025-07-03 11:58:54 -0400

    create summary-name page

---
+++ src/app/pages/summary-name/summary-name.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-summary-name',
+  standalone: true,
+  imports: [],
+  templateUrl: './summary-name.component.html',
+  styles: ``
+})
+export class SummaryNameComponent {
+
+}
